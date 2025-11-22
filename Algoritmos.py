a0f8256f71142a5c1039ff8e8510f37732c70ff8
Steven <steven@alunos.utfpr.edu.br>
2025-11-22 15:25:26 -0300

    Corrigido problema no FIFO

--- Algoritmos.py
+++ Algoritmos.py
@@ -19,9 +19,8 @@ def PrioP(tarefas, tempoMax, quantum, alpha):
    return Escalonador(tarefas, tempoMax, quantum, alpha, True, algoritmoSelecionado=lambda prontas: PegarMaiorPrioridade(prontas))
 
 # Algoritmo PrioEnv, não é afetado pelo quantum
-def PrioEnv(tarefas, tempoMax, quantum,alpha):
-    
-    return None
+def PrioEnv(tarefas, tempoMax, quantum,alpha):    
+    return 
 
 def Escalonador(tarefas, tempoMax, quantum, alpha, usarCopia, algoritmoSelecionado):  
     copiaTarefas = tarefas.copy() # Cria uma copia das tarefas para não alterar o conteudo original
@@ -29,84 +28,53 @@ def Escalonador(tarefas, tempoMax, quantum, alpha, usarCopia, algoritmoSeleciona
     instrucoes = [] # Lista de instrucoes contento informações uteis para gerar o gráfico
     tarefasInativas = [] # Lista para guardar as instrucoes inativas
     tarefasProntas = [] # Lista de prontas para auxiliar e facilitar na manipulação do que deve ser selecionado
-    tarefasProntas = PegarTarefasProntas(copiaTarefas, tarefasProntas,tempoAtual, False) # Procura por tarefas que já estão ingressadas e possuem duracao restante
-
-    while tempoAtual < tempoMax: # Loop para garantir que a lista de instrucao gerada tenha esvaziado as duracoes das tarefas
-        for i in range(quantum): # Loop para garantir o funcionamento do quantum, sempre que ele acaba é reiniciado o processo de seleção da proxima tarefa
+    tarefasProntas = PegarTarefasProntas(copiaTarefas, tarefasProntas,tempoAtual) # Procura por tarefas que já estão ingressadas e possuem duracao restante
+    
+    while tempoAtual < tempoMax: # Loop para garantir que a lista de instrucao gerada tenha esvaziado as duracoes das tarefas        
+        removido = False # Para previnir que uma tarefa seja removida duas vezes quando o quantum acaba e a tarefa é concluída ao mesmo tempo
+        for i in range(quantum): # Loop para garantir o funcionamento do quantum, sempre que ele acaba é reiniciado o processo de seleção da proxima tarefa            
+            
             tarefa = algoritmoSelecionado(tarefasProntas)
             instrucoes.append(tarefa) # Adiciona a tarefa na lista de instrucoes
             tarefasInativas.extend(PegarTarefasInativas(copiaTarefas, tarefa, tempoAtual)) # Pega e adiciona todas as tarefas que ja estao ingressados no
-
+            
             tarefa['duracaoRestante'] -= 1 # Modifica a tarefa que foi 'processado' diminuido sua duracao
+            
+            if tarefa['duracaoRestante'] == 0:
+                tarefasProntas = [t for t in tarefasProntas if t['duracaoRestante'] > 0] 
+                removido = True
+
             tempoAtual+=1 # Aumenta o tempo atual da simulação
-            tarefasProntas = PegarTarefasProntas(copiaTarefas, tarefasProntas,tempoAtual, usarCopia) # Procura por novas tarefas prontas no tempo atual atualizado,
-            
-            
+            tarefasProntas = PegarTarefasProntas(copiaTarefas, tarefasProntas,tempoAtual) # Procura por novas tarefas prontas no tempo atual atualizado,
+                        
             if tempoAtual >= tempoMax:
                 break
             
-        if not usarCopia:
+        if not usarCopia and len(tarefasProntas) > 0 and not removido:
             tarefasProntas.pop(0) # Remove o primeiro da lista de prontos a tarefa quando o quantum dele acaba, impedindo que ele rode até o final
-         
+
     return instrucoes, tarefasInativas
 
 
 def EnvelhecerPrioridades(alpha,tarefasProntas, tarefasAtual, tarefasOriginais, tempoAtual, tarefaAnteriorID, concluida):    
-    
-    envelhecer = False
-    if concluida:
-        envelhecer = True
-        concluida = False
-    print("Tempo:", tempoAtual)
-    # Procura por tarefas que:
-    # ingressaram no tempo atual,
-    # foi concluida
-    for tarefa in tarefasProntas:
-        if (tarefa['ingressoTarefa'] == tempoAtual):
-            envelhecer = True
-            print("entrou novo", tempoAtual)
-        if tarefa['duracaoRestante'] == 0:
-            concluida = True
-            print("concluida", tempoAtual)
-    
-
-    if envelhecer:
-        for tarefa in tarefasProntas:            
-            tarefa['prioridade'] += alpha  # Aumenta a prioridade da tarefa (envelhece)
-            print(f"Tarefa {tarefa['id']} envelheceu para prioridade {tarefa['prioridade']} no tempo {tempoAtual}")
-        print("----")
-    
-    if tarefaAnteriorID != -1 and tarefasAtual['id'] != tarefaAnteriorID:        
-        indice = next((i for i, t in enumerate(tarefasOriginais) if t['id'] == tarefasAtual['id']), None)    
-        tarefasAtual['prioridade'] = tarefasOriginais[indice]['prioridade'] 
-        print(tarefasAtual['id'], " prioridade resetada para ", tarefasAtual['prioridade'], " no tempo ", tempoAtual)        
-
-    print("=====")
-    return concluida
+    return None
 
 # Funcao para buscar por tarefas ingressadas e contendo duracao restante
-def PegarTarefasProntas(tarefas, tarefasProntas, tempoAtual, apagarConcluidas):
+def PegarTarefasProntas(tarefas, tarefasProntas, tempoAtual):
     # Faz uma copia das tarefas prontas existente para não altera-la ao retornar,
     # Necessário para o funcionamento correto de uma FILA para o FIFO
     prontas = list(tarefasProntas)
-
-    print("Antes Tempo Atual:", tempoAtual)
-    for t in prontas:
-        print(t)
+    
     # Condição para remover tarefas conlcuidas com duracao restante em 0
     # Necesssário para o funcionamento do SRTF e PrioP
-    if apagarConcluidas: 
-        prontas = [t for t in prontas if t['duracaoRestante'] > 0]
     
-    print("Depois Tempo Atual:", tempoAtual)
-    for t in prontas:
-        print(t)
-    print("-------------")
+    prontas = [t for t in prontas if t['duracaoRestante'] > 0]        
     idsProntas = [t['id'] for t in prontas] # Lista para guardar os IDs das tarefas já presentes na lista de prontas
     # Loop para buscar por tarefas ingressadas e com duracao restante, evitando também tarefas iguais
     for tarefa in tarefas:
         if tarefa['id'] not in idsProntas and ChecarIngresso(tarefa, tempoAtual) and tarefa['duracaoRestante'] > 0:
             prontas.append(tarefa) # Adiciona a tarefa na lista de prontas caso passe nas condições
+    
     
     return prontas
 
